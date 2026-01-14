4f6079d37a22c859c4e028f3330d000eca7e013e
Vincent Guiu <vincent.guiu+8746@comarch.com>
2026-01-14 12:58:21 +0100

    commit local resources

---
+++ pandora/categories.gd
@@ -0,0 +1,17 @@
+# Do not modify! Auto-generated file.
+class_name PandoraCategories
+
+
+const ITEM = "1"
+const RECIPES = "3"
+const ITEMSTACK = "8"
+
+
+class ItemCategories:
+	const RECIPES = "2"
+
+
+class RecipesCategories:
+	const LEVEL_1 = "17"
+
+
--- scripts/sync_addons.bash
+++ scripts/sync_addons.bash
@@ -14,6 +14,7 @@ GITIGNORE=$(realpath --relative-to=$PWD ${PROJECT_ROOT}/.gitignore)
 
 ADDONS_LIST=(
     git@github.com:titaniteChuck/Godot-Commons-Addons.git
+    git@github.com:bitbrain/pandora.git
     git@github.com:huwpascoe/godot_resource_path.git # used to store resources UIDs, that follow file move
     # git@github.com:EiTaNBaRiBoA/JsonClassConverter.git # used by the save_maager to convert to/from json
 )
